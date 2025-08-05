a33458555af3bc7f46c33dce7821d7bf209d29a6
jakubforman <me@jakubforman.eu>
2025-08-05 20:48:19 +0200

    Implement drag-and-drop reorder functionality in Tab 1 page.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {Component} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonReorder,
+  IonReorderGroup, IonList
+} from '@ionic/angular/standalone';
+import {ExploreContainerComponent} from '../explore-container/explore-container.component';
+import {ReorderEndCustomEvent} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonItem, IonLabel, IonReorder, IonReorderGroup, IonList],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+  }
+
+  handleReorderEnd(event: ReorderEndCustomEvent) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group.
+    event.detail.complete();
+  }
 }
